08f4e54111fb8e9cc64c43badea25a0838b41165
Montero Charm Venice <160820060+MonteroCharmVenice-BSIT32E1@users.noreply.github.com>
2024-05-04 09:42:09 +0800

    installed Microsoft.AspNetCore.Authentication.JwtBearer

--- AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a953fe0687bff7b5e4dd866361df39b7f3a3773c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdbfaadd5a1ed8480aea58f4c3c86eb1ebf86f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

920e3a295f69bc31d9c34f38f776c15917dce983
Montero Charm Venice <160820060+MonteroCharmVenice-BSIT32E1@users.noreply.github.com>
2024-05-04 10:04:54 +0800

    add code to secret.cs

---
+++ AuthServer/Secret.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthServer
+{
+    public static class Secret
+    {
+        public static readonly string JwtSigningKey = "charm_venice_montero_0123456789_11";
+    }
+}
--- AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+168a441468da7af37f0b964d4e922580a2d52ef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867b75886e8206526a774824d7134936e1d8e779")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

00ac87ee500638fca2a35351b25a3297b9492dad
Montero Charm Venice <160820060+MonteroCharmVenice-BSIT32E1@users.noreply.github.com>
2024-05-04 09:39:19 +0800

    SET UP

---
+++ PREFINALS_AUTH_MONTERO_CHARM-VENICE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_MONTERO_CHARM_VENICE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
